ad41cfadb8f1fdbff098b36f494dd51b708b1b05
Jaynaywilliams <36012612+Jaynaywilliams@users.noreply.github.com>
2026-04-30 10:56:11 -0700

    Create firebase.js

---
+++ src/services/firebase.js
@@ -0,0 +1,18 @@
+import admin from "firebase-admin";
+
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT env variable is missing");
+}
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
+}
+
+export const db = admin.firestore();
+export { admin };
