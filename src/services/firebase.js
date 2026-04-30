5069f2f6abc08a40fde5eaad36307491c1927f5a
Jaynaywilliams <36012612+Jaynaywilliams@users.noreply.github.com>
2026-04-30 11:13:10 -0700

    Update firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-onsole.log("🔥 Firebase initialized");
+console.log("🔥 Firebase initialized");
 
 if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
   throw new Error("FIREBASE_SERVICE_ACCOUNT env variable is missing");
